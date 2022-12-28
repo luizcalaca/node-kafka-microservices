3bf2cb21c0191f3dc853daaaa7f6f7161ae8a644
Luiz Calaça <luizcalaca@gmail.com>
2022-12-28 17:21:56 -0300

    fix: fix :)

--- app1/index.js
+++ app1/index.js
@@ -1,36 +1,23 @@
 const express = require('express')
 const kafka = require('kafka-node')
 const app = express()
-const sequelize = require('sequelize')
-const db = new sequelize(process.env.POSTGRES_URL)
-
-const User = db.define('user', {
-    name: sequelize.STRING,
-    email: sequelize.STRING,
-    password: sequelize.STRING
-})
-db.sync({force: true})
-
 app.use(express.json())
 
 const isRunning = () => {
     const client = new kafka.KafkaClient({kafkaHost: process.env.KAFKA_BOOTSTRAP_SERVERS})
     const producer = new kafka.Producer(client)
+    
     producer.on('ready', () => {
-        console.log('producer ready')
         app.post('/', async (req, res) => {
             producer.send([{topic: process.env.KAFKA_TOPIC, messages: JSON.stringify(req.body)}], async (err, data) => {
-                if(err) console.log(err)
+                if(err) res.send(err)
                 else {
-                    await User.create(req.body);
                     res.send(req.body)
                 } 
             })
         })
     })
 }
-setTimeout(isRunning, 5000)
-
-
+setTimeout(isRunning, 1000)
 
 app.listen(process.env.PORT)
