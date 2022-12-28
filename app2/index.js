2f64ce42977d0327f58f093480a47298bb8eee4e
Luiz Calaça <luizcalaca@gmail.com>
2022-12-28 15:45:35 -0300

    refactor: creating a settimeout

--- app2/index.js
+++ app2/index.js
@@ -3,28 +3,32 @@ const kafka = require('kafka-node')
 const { default: mongoose } = require('mongoose')
 const app = express()
 
-mongoose.connect(process.env.MONGO_URL)
-const User = new mongoose.model('user',{
-    name: String,
-    email: String,
-    password: String
-})
+const isRunning = () => {
+    mongoose.connect(process.env.MONGO_URL)
+    const User = new mongoose.model('user',{
+        name: String,
+        email: String,
+        password: String
+    })
 
-app.use(express.json())
+    app.use(express.json())
 
-const client = new kafka.KafkaClient({kafkaHost: process.env.KAFKA_BOOTSTRAP_SERVERS})
-const consumer = new kafka.Consumer(client, [{topic: process.env.KAFKA_TOPIC}], {
-    autoCommit: false
-})
+    const client = new kafka.KafkaClient({kafkaHost: process.env.KAFKA_BOOTSTRAP_SERVERS})
+    const consumer = new kafka.Consumer(client, [{topic: process.env.KAFKA_TOPIC}], {
+        autoCommit: false
+    })
 
-consumer.on('message', async(message) => {
-    const user = new User(JSON.parse(message.value))
-    await user.save()
-    
-})
+    consumer.on('message', async(message) => {
+        const user = new User(JSON.parse(message.value))
+        await user.save()
+        
+    })
 
-consumer.on('error', (err) => {
-    console.log(err)
-})
+    consumer.on('error', (err) => {
+        console.log(err)
+    })
+}
+setTimeout(isRunning, 5000)
+
 
 app.listen(process.env.PORT)
