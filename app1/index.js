03e3301e53710815c568e8e1fa207fe24f15e163
Luiz Calaça <luizcalaca@gmail.com>
2022-12-28 13:12:00 -0300

    feat: microservices + kafka + PostgreSQL + Sequelize

---
+++ app1/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const kafka = require('kafka-node')
+const app = express()
+const sequelize = require('sequelize')
+const db = new sequelize(process.env.POSTGRES_URL)
+
+const User = db.define('user', {
+    name: sequelize.STRING,
+    email: sequelize.STRING,
+    password: sequelize.STRING
+})
+db.sync({forcef:true})
+
+app.use(express.json())
+
+const client = new kafka.KafkaClient({kafkaHost: process.env.KAFKA_BOOTSTRAP_SERVERS})
+const producer = new kafka.Producer(client)
+producer.on('ready', () => {
+    console.log('producer ready')
+    app.post('/', async (req, res) => {
+        producer.send([{topic: process.env.KAFKA_TOPIC, messages: JSON.stringify(req.body)}], async (err, data) => {
+            if(err) console.log(err)
+            else {
+                await User.create(req.body);
+                res.send(req.body)
+            } 
+        })
+    })
+})
+
+
+
+app.listen(process.env.PORT)
